05393248998fe2d991b980e58f329e9eb38aa7fe
bradcsweet <bradcsweet@gmail.com>
2017-10-11 14:09:47 -0400

    automobile fields and properties

--- Week3_Day3_Inheritance/Automobile.cs
+++ Week3_Day3_Inheritance/Automobile.cs
@@ -8,6 +8,19 @@ namespace Week3_Day3_Inheritance
 {
     class Automobile : Vehicle
     {
+        private int numberOfWheels;
+        private float wheelSize;
+        private int numberOfDoors;
+
+        public int NumberOfWheels
+        {
+            get { return this.numberOfWheels; }
+        }
+
+        public Automobile(int numberOfWheels, float wheelSize, int numberOfDoors)
+        {
+
+        }
 
 
     }
--- Week3_Day3_Inheritance/Vehicle.cs
+++ Week3_Day3_Inheritance/Vehicle.cs
@@ -8,13 +8,13 @@ namespace Week3_Day3_Inheritance
 {
     class Vehicle
     {
-        private int seats;
-        private int carryingCapacity;
-        private string color;
-        private double movementSpeed;
+        protected int seats;
+        protected int carryingCapacity;
+        protected string color;
+        protected double movementSpeed;
         private float gearRatio;
         private float engineSize;
-        private double distanceTraveled;
+        protected double distanceTraveled;
 
         public int Seats
         {
