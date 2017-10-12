b70976209740d0b6eb2d2c32cdb71fac039e5662
bradcsweet <bradcsweet@gmail.com>
2017-10-12 10:20:26 -0400

    Changed vehicle class to abstract

--- Week3_Day3_Inheritance/Vehicle.cs
+++ Week3_Day3_Inheritance/Vehicle.cs
@@ -58,6 +58,8 @@ namespace Week3_Day3_Inheritance
             distanceTraveled += movementSpeed;
         }
 
+        //public abstract void StartEngine(int keycode); // this is just an example of a very seldom used tactic to force 
+                                                         //child class developers to write their own method for StartEngine()
 
     }
 
